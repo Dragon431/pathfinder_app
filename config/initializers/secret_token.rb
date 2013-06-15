c4555c52c225fe16a1d684ab8dea293984503f94
Dragon431 <wingzero431@gmail.com>
2013-06-15 14:24:44 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PathfinderApp::Application.config.secret_token = 'f5c762c166a8aa5ecbb293fb49a976406a7314f02a0c23fb0563c21fc2b29a302ef89286168397148891514f406d28f7da787d415d67d2934063c4fe15c1498e'
