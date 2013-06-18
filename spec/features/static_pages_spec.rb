803117e93373d9de5e39b33fab26c105f6776de2
Dragon431 <wingzero431@gmail.com>
2013-06-17 19:13:48 -0500

    Finish layout and routes

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,41 +1,34 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-	subject { page	}
+	subject { page }
 
 	describe "Home page" do
-		it "should have the h1 'Pathfinder App'" do
-			visit '/static_pages/home'
-			page.should have_selector('h1', text: 'Pathfinder App')
-		end
+		before { visit root_path }
 
-		it "should have the title 'Home'" do
-			visit '/static_pages/home'
-			page.should has_title? 'Home'
-		end
+		it { should have_selector('h1', text: 'Pathfinder App') }
+		it { has_title? full_title('') }
+		it { has_no_title? '| Home' }
 	end
 
 	describe "Help page" do
-		it "should have the h1 'Help'" do
-			visit '/static_pages/help'
-			page.should have_selector('h1', text: 'Help')
-		end
+		before { visit help_path }
 
-		it "should have the title 'Help'" do
-			visit '/static_pages/help'
-			page.should has_title? 'Help'
-		end
+		it { should have_selector('h1', text: 'Help') }
+		it { has_title? full_title('Help') }
 	end
 
 	describe "About page" do
-		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
-			page.should have_selector('h1', text: 'About Us')
-		end
+		before { visit about_path }
 
-		it "should have the title 'About'" do
-			visit '/static_pages/home'
-			page.should has_title? 'About'
-		end
+		it { should have_selector('h1', text: 'About Us') }
+		it { has_title? full_title('About Us') }
+	end
+
+	describe "Contact page" do
+		before { visit contact_path }
+
+		it { should have_selector('h1', text: 'Contact') }
+		it { has_title? full_title('Contact') }
 	end
 end
