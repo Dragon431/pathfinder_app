4e2abeebecdbe245ef56c6cad4e498b436c8908e
Dragon431 <wingzero431@gmail.com>
2013-06-24 18:02:34 -0500

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+	before_filter :signed_in_user,  only: [:edit, :update, :destroy]
+	before_filter :correct_user,    only: [:edit, :update]
+	before_filter :admin_user,      only: [:index, :destroy]
+
   def new
 	  @user = User.new
   end
@@ -11,10 +15,49 @@ class UsersController < ApplicationController
 		@user = User.new(params[:user])
 
 		if @user.save
+			sign_in @user
 			flash[:success] = "Welcome to the Pathfinder App!"
 			redirect_to @user
 		else
 			render 'new'
 		end
 	end
+
+	def edit
+	end
+
+	def update
+		if @user.update_attributes(params[:user])
+			sign_in @user
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
+	def index
+		@users = User.paginate(page: params[:page])
+	end
+
+	def destroy
+		User.find(params[:id]).destroy
+		redirect_to users_url
+	end
+
+	private
+		def signed_in_user
+			unless signed_in?
+				store_location
+				redirect_to signin_url
+			end
+		end
+
+		def correct_user
+			@user = User.find(params[:id])
+			redirect_to(root_path) unless current_user?(@user)
+		end
+
+		def admin_user
+			redirect_to(root_path) unless current_user.admin?
+		end
 end
